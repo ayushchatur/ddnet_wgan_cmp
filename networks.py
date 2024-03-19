183d40a86a9c9c09ce5a5005d14b120bc05ddb2b
Ayush Chaturvedi <ayush.6200@gmail.com>
2024-03-19 00:15:50 -0500

    fixing tensor device errors for fake_

--- networks.py
+++ networks.py
@@ -116,9 +116,9 @@ class WGAN_VGG(nn.Module):
         a = a.type(torch.float32).to(device = y.device)
         interp = (a*y + ((1-a)*fake)).requires_grad_(True)
         d_interp = self.discriminator(interp)
-        fake_ = torch.FloatTensor(y.shape[0], 1).fill_(1.0).requires_grad_(False)
+        # fake_ = torch.FloatTensor(y.shape[0], 1).fill_(1.0).requires_grad_(False)
 
-        fake2 = torch.ones((y.shape[0], 1), dtype=torch.float32, device=y.device, requires_grad=False)
+        fake_ = torch.ones((y.shape[0], 1), dtype=torch.float32, device=y.device, requires_grad=False)
         # fake2 = fake_.fill_(1.0)
         gradients = torch.autograd.grad(
             outputs=d_interp, inputs=interp, grad_outputs=fake_,
